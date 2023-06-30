f2cc971bd7bf5ded3cccb0d088c00d0beaade236
Suman <dr.suman1jan@gmail.com>
2023-06-30 14:36:50 -0500

    bonus mission done

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ window.addEventListener("load", function() {
     this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
        return response.json();
         }).then (function(response){
+      let sortedAstronauts = response.sort(function(a, b) {return b.hoursInSpace - a.hoursInSpace});
+
 let myContainer = document.getElementById("container");
 let allMyHtml = "";
+
+let myCount = `
+     <h3>Astronaut Count: ${sortedAstronauts.length}</h3>`
+     allMyHtml += myCount;
+
             for (let i=0;i<response.length;i++){
                 //console.log(response[i]);
                 let myHtml =`
@@ -12,8 +19,8 @@ let allMyHtml = "";
                 <div class = "bio">
                 <h3> ${response[i].firstName} ${response[i].lastName}</h3>
                 <ul>
-                <li> hoursInSpace:${response[i].hoursInSpace}</li>
-                <li> Active:${response[i].active}</li>
+                <li> Hours in Space:${response[i].hoursInSpace}</li>
+                <li ${response[i].active ? 'style="color:green;"': ''}>Active: ${response[i].active}</li>
                 <li> Skills:${response[i].skills}</li>
                 </ul>
                </div>
